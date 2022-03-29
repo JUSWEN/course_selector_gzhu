3dd2c90476c559dfcdfcea035ca2c4984a72b0e1
Lihao Deng <lihao-deng@outlook.com>
2022-03-29 10:44:16 +0800

    1

--- run.py
+++ run.py
@@ -3,8 +3,9 @@
 # 邮箱 lihao_deng@qq.com
 # 本人不对因使用此脚本而产生的任何后果或损失负责
 
-from courseSelector import main
 from multiprocessing import freeze_support
+
+from courseSelector import main
 
 freeze_support()
 main.main()
