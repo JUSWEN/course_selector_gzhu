e958d520caee680997b545c51a0aed40ada12b64
Lihao Deng <lihao-deng@outlook.com>
2022-04-02 19:41:11 +0800

    1

--- courseSelector/main.py
+++ courseSelector/main.py
@@ -1,11 +1,10 @@
 import os
 import re
 import sys
-from multiprocessing import Process
 
 from selenium.webdriver.common.by import By
 
-from . import cookies_prepare, qiangke, wd_login, xuehao_mima
+from . import xuehao_mima, wd_login
 
 
 def main():
@@ -65,17 +64,10 @@ def main():
                   "如果直接Enter，则延时为0。抢课进程将在延时的分钟数后开始执行，\n"
                   "另一个进程将立即执行，登陆教务系统，维持会话并定时更新cookie\n"
                   "请输入延时分钟数：")
-    
+
     if delay == '':
         delay = 0
     else:
-        delay = int(delay)*60
+        delay = int(delay) * 60
 
-    # 创建两个进程，一个用更新cookie以及维持会话， 一个用来发送表单抢课
-    process = [
-        Process(target=cookies_prepare.cookies_prepare, args=(xuhao, mima)),
-        Process(target=qiangke.qiangke, args=(xuhao, delay))
-    ]
-
-    [p.start() for p in process]
-    [p.join() for p in process]
+    return (xuhao, mima, delay)
--- run.py
+++ run.py
@@ -3,9 +3,20 @@
 # 邮箱 lihao_deng@qq.com
 # 本人不对因使用此脚本而产生的任何后果或损失负责
 
-from multiprocessing import freeze_support
+from multiprocessing import Process, freeze_support
 
-from courseSelector import main
+from courseSelector import cookies_prepare, main, qiangke
 
-freeze_support()
-main.main()
+if __name__ == "__main__":
+    freeze_support()
+
+    xuhao, mima, delay = main.main()
+
+    # 创建两个进程，一个用更新cookie以及维持会话， 一个用来发送表单抢课
+    process = [
+        Process(target=cookies_prepare.cookies_prepare, args=(xuhao, mima)),
+        Process(target=qiangke.qiangke, args=(xuhao, delay))
+    ]
+
+    [p.start() for p in process]
+    [p.join() for p in process]
