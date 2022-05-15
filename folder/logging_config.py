1859afc98479ffaeb356bb929de88f3bfeab4953
Lihao Deng <lihao-deng@outlook.com>
2022-05-15 16:37:15 +0800

    1

--- folder/logging_config.py
+++ folder/logging_config.py
@@ -12,9 +12,10 @@ def configure_logger():
     f_handler.setLevel(logging.DEBUG)
 
     # Create formatters and add it to handlers
-    c_format = logging.Formatter('%(levelname)s - %(threadName)s - %(message)s')
+    c_format = logging.Formatter(
+        ' %(threadName)s -%(levelname)s - %(message)s')
     f_format = logging.Formatter(
-        '%(asctime)s  - %(levelname)s -%(threadName)s - %(message)s')
+        '%(asctime)s  -%(threadName)s - %(levelname)s - %(message)s')
     c_handler.setFormatter(c_format)
     f_handler.setFormatter(f_format)
 
