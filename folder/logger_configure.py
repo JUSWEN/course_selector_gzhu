4be1869ae5dbaae0de98e556a441208b39e58d12
Lihao Deng <lihao-deng@outlook.com>
2022-05-18 18:40:02 +0800

    Minor modification

--- folder/logger_configure.py
+++ folder/logger_configure.py
@@ -9,12 +9,16 @@ def configure_logger():
     file = open(file_name, "w")
     file.close()
 
+    logger.remove()
     logger.add(sys.stderr,
-               format="{process} {level} {message}",
-               filter="my_module",
+               colorize=True,
+               enqueue=True,
+               format="<level>{level}</level>| "
+               "<magenta>{time:YYYY-MM-DD}</magenta>| "
+               "<level>{message}</level>",
                level="INFO")
     logger.add(file_name,
-               format="{time} {process} {level} {message}",
+               format="{time}| {process}| {level}| {message}",
                enqueue=True,
                encoding='utf-8',
                level="DEBUG")
