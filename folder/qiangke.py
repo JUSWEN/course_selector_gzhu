8a79144776ccb552d05dbccd2bfe9eaba1150797
Lihao Deng <lihao-deng@outlook.com>
2022-05-18 15:36:45 +0800

    Minor modification

--- folder/qiangke.py
+++ folder/qiangke.py
@@ -10,83 +10,86 @@ from . import logger_configure, submit_data
 
 
 def qiangke(student_number, delay):
-    logger = logger_configure.get_logger()
-
-    time.sleep(delay)
-
-    logger.info('开始抢课！')
-
-    cookie_txt = os.path.exists('./cookies.txt')
-
-    # 如果不存在cookie文件
-    if not cookie_txt:
-        while True:
-            time.sleep(10)
-
-            cookie_txt = os.path.exists('./cookies.txt')
-            if cookie_txt:
-                break
-
-    with open('./cookies.txt', 'r') as file:
-        listcookies = json.loads(file.read())
-
-    jar = RequestsCookieJar()
-    jar.set(listcookies[0]['name'], listcookies[0]['value'])
-
-    # 读取保存的data表单,并通过async异步的方式发送
-    with open('./data.txt', 'r') as data_file:
-        tobeprocessed_data = data_file.read()
-
-    datas = tobeprocessed_data.split('\n')
-
-    while True:
-        tasks = []
-
-        n = 0
-
-        while n < 10:
-            i = 0
-
-            while i < len(datas) - 1:
-                data = eval(datas[i])
-
-                coroutine = submit_data.submit_data(student_number, data, jar,
-                                                    logger)
-                task = asyncio.ensure_future(coroutine)
-                tasks.append(task)
-
-                i += 1
-
-            n += 1
-
-        loop = asyncio.get_event_loop()
-        result_list = loop.run_until_complete(asyncio.wait(tasks))
-
-        # i表示cookie失效，需要重新发包
-        i = 0
-
-        for results in result_list:
-            for result in results:
-                if result.result() == 'cookie out of date':
-                    i = 1
-
+    try:
+        logger = logger_configure.get_logger()
+    
+        time.sleep(delay)
+    
+        logger.info('开始抢课！')
+    
+        cookie_txt = os.path.exists('./cookies.txt')
+    
+        # 如果不存在cookie文件
+        if not cookie_txt:
+            while True:
+                time.sleep(10)
+    
+                cookie_txt = os.path.exists('./cookies.txt')
+                if cookie_txt:
                     break
-
+    
+        with open('./cookies.txt', 'r') as file:
+            listcookies = json.loads(file.read())
+    
+        jar = RequestsCookieJar()
+        jar.set(listcookies[0]['name'], listcookies[0]['value'])
+    
+        # 读取保存的data表单,并通过async异步的方式发送
+        with open('./data.txt', 'r') as data_file:
+            tobeprocessed_data = data_file.read()
+    
+        datas = tobeprocessed_data.split('\n')
+    
+        while True:
+            tasks = []
+    
+            n = 0
+    
+            while n < 10:
+                i = 0
+    
+                while i < len(datas) - 1:
+                    data = eval(datas[i])
+    
+                    coroutine = submit_data.submit_data(student_number, data, jar,
+                                                        logger)
+                    task = asyncio.ensure_future(coroutine)
+                    tasks.append(task)
+    
+                    i += 1
+    
+                n += 1
+    
+            loop = asyncio.get_event_loop()
+            result_list = loop.run_until_complete(asyncio.wait(tasks))
+    
+            # i表示cookie失效，需要重新发包
+            i = 0
+    
+            for results in result_list:
+                for result in results:
+                    if result.result() == 'cookie out of date':
+                        i = 1
+    
+                        break
+    
+                if i:
+                    break
+    
             if i:
+                with open('./cookies.txt', 'r') as file:
+                    listcookies = json.loads(file.read())
+    
+                jar = RequestsCookieJar()
+                jar.set(listcookies[0]['name'], listcookies[0]['value'])
+            else:
                 break
-
-        if i:
-            with open('./cookies.txt', 'r') as file:
-                listcookies = json.loads(file.read())
-
-            jar = RequestsCookieJar()
-            jar.set(listcookies[0]['name'], listcookies[0]['value'])
-        else:
-            break
-
-    loop.close()
-
-    logger.info('抢课结束！')
-
-    input("程序运行结束，回车以退出程序")
-    sys.exit(0)
+    
+        loop.close()
+    
+        logger.info('抢课结束！')
+    
+        input("程序运行结束，回车以退出程序")
+        sys.exit(0)
+    except Exception as e:
+        logger.error(e)
